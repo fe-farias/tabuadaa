86b54baa0d4eb9a775d31c2ccfa22bba660be7b1
Felipe Francisco <fefarias97@gmail.com>
2021-11-27 14:51:31 -0300

    Resolução tabuada

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+ int N, contador = 1;
+
+Console.Write("Tabuada do: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+while(contador<=10){
+    Console.WriteLine($"{N} x {contador} = {N*contador} ");
+    contador += 1;
+}
